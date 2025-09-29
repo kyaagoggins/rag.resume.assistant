3b4ede0a642dbadace2f1cb77a5ece34e2f8042d
Kyaa Goggins <kyaagoggins@gmail.com>
2025-09-29 19:00:12 -0400

    testing and angular app initial

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',     
   standalone: true,
-  imports: [NavbarComponent],           
+  imports: [
+    //NavbarComponent,
+    RouterModule
+  ],           
   templateUrl: './app.component.html',       
   styleUrls: ['./app.component.css']        
 })
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,22 +1,12 @@
-/// <reference types="@angular/localize" />
 
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
-
-// Material + NG Bootstrap
-import { MatButtonModule } from '@angular/material/button';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { importProvidersFrom } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideAnimations(),
-    provideHttpClient(),
-    importProvidersFrom(
-      MatButtonModule,
-      NgbModule
-    )
+    importProvidersFrom(RouterModule)  
   ]
-}).catch(err => console.error(err));
+})
+  .catch(err => console.error(err));
